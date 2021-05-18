452f31bd9f56cc4f636f1a5d4b716213416f5e30
Forrest Morrisey <forrestmorrisey@gmail.com>
2021-05-18 15:31:28 -0500

    Proj boilerplate, removed excess, init code

---
+++ src/components/thumbs/ThumbsUpButton.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ThumbsUpButton() {
+  return (
+    <div>
+      <button type="button">LIKE ME!</button>
+    </div>
+  );
+}
+
+export default ThumbsUpButton;
